575ae152fed83d44f0f4656b016252c87f57e0b5
TS810 <eehd810@gmail.com>
2025-08-10 11:58:29 +0900

    config edit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import fs from 'fs'
 import path from 'path'
 
 export default defineConfig({
-  base: '/t-time/', // <-- 배포 전 REPO_NAME으로 바꿔주세요. (커스텀 도메인 사용 시 '/' 로 변경)
+  base: '/', // <-- 배포 전 REPO_NAME으로 바꿔주세요. (커스텀 도메인 사용 시 '/' 로 변경)
   plugins: [react()],
   // 아래 server.https 블록은 로컬에서 HTTPS 개발용(인증서 파일 필요)입니다.
   // 인증서 없으면 이 블록을 제거하거나 주석 처리하세요.
